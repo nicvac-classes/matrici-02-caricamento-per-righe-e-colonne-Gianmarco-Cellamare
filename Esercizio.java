e2c445e79628eeca649e669909d1e558efb33424
Gianmarco-Cellamare <gianmarco.cellamare@marconibari.edu.it>
2025-11-03 22:43:00 +0000

    esercizio2

--- Esercizio.java
+++ Esercizio.java
@@ -2,14 +2,51 @@
 public class Esercizio {
 
     public static void riempiPerRighe(/* scrivere qui i parametri formali */) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
+        int conta = 0;
+        int i;
+        int conta;
+        i=0;
+        while (i <= RIGHE-1;) {
+            j=0;
+            while (j <= COLONNE-1;) {
+               conta = conta + 1;
+               M[i][j] = conta;
+               j=j+1
+             }
+             i=i+1;
+        }
     }
 
     public static void riempiPerColonne(/* scrivere qui i parametri formali */) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
-    }
+        int conta = 0;
+        int i;
+        int conta;
+        i=0;
+        while (j <= COLONNE-1;) {
+            i=0;
+            while (i <= RIGHE-1;) {
+               conta = conta + 1;
+               M[i][j] = conta;
+               i=i+1
+             }
+             j=j+1;
+        }
+
+  }
 
     public static void main(String[] args) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
+          int RIGHE = 3;
+          int COLONNE = 5;
+          int[][] matricePerRighe = new int[RIGHE][COLONNE];
+          int[][] matricePerColonne = new int[RIGHE][COLONNE];
+  
+          riempiPerRighe(matricePerRighe, RIGHE, COLONNE);
+          riempiPerColonne(matricePerColonne, RIGHE, COLONNE);
+  
+          System.out.println("Per righe:");
+          UtilsMatrice.visualizza(matricePerRighe);
+ 
+          System.out.println("Per colonne:");
+          UtilsMatrice.visualizza(matricePerColonne);
     }
 }
